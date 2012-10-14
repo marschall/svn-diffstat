8298106e2a255b9c9f9dbe29bbb03997a396c7f3
Philippe Marschall <philippe.marschall@gmail.com>
2012-10-14 22:53:32 +0200

    bump svn client

--- src/main/java/com/github/marschal/svndiffstat/Main.java
+++ src/main/java/com/github/marschal/svndiffstat/Main.java
@@ -1,9 +1,17 @@
 package com.github.marschal.svndiffstat;
 
+import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		FSRepositoryFactory.setup();
+		SVNClientManager clientManager = SVNClientManager.newInstance();
+		
+		clientManager.getLogClient();
+		clientManager.getDiffClient();
 	}
 
 }
