24c610cd109f470b6639845ff2b9814a38a1b668
Philippe Marschall <philippe.marschall@gmail.com>
2014-06-03 21:21:29 +0200

    formatting

--- src/main/java/com/github/marschal/svndiffstat/AbbreviatingNumberFormat.java
+++ src/main/java/com/github/marschal/svndiffstat/AbbreviatingNumberFormat.java
@@ -1,5 +1,7 @@
 package com.github.marschal.svndiffstat;
 
+import static java.lang.Math.abs;
+
 import java.text.FieldPosition;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
@@ -13,18 +15,19 @@ final class AbbreviatingNumberFormat extends NumberFormat {
 
   @Override
   public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
-    long toFormat = Math.abs(number);
+    long toFormat = abs(number);
     if (toFormat < 0) {
-      // abs(Long.MIN_VALUE) = abs(Long.MIN_VALUE)
+      // Java Puzzler:
+      // abs(Long.MIN_VALUE) = Long.MIN_VALUE
       toFormat = Long.MAX_VALUE;
     }
-    if (toFormat < 1000L) {
+    if (toFormat < 1_000L) {
       toAppendTo.append(toFormat);
-    } else if (toFormat < 1000000L) {
-      long thousands = toFormat / 1000L;
+    } else if (toFormat < 1_000_000L) {
+      long thousands = toFormat / 1_000L;
       toAppendTo.append(thousands);
 
-      long reminder = toFormat - (thousands * 1000L);
+      long reminder = toFormat - (thousands * 1_000L);
       if (reminder != 0L && reminder >= 100L) {
         toAppendTo.append('.');
         toAppendTo.append(reminder / 100L);
@@ -32,13 +35,13 @@ final class AbbreviatingNumberFormat extends NumberFormat {
       toAppendTo.append('k');
 
     } else {
-      long millions = toFormat / 1000000L;
+      long millions = toFormat / 1_000_000L;
       toAppendTo.append(millions);
 
-      long reminder = toFormat - (millions * 1000000L);
-      if (reminder != 0L && reminder >= 100000L) {
+      long remainder = toFormat - (millions * 1_000_000L);
+      if (remainder != 0L && remainder >= 100_000L) {
         toAppendTo.append('.');
-        toAppendTo.append(reminder / 100000L);
+        toAppendTo.append(remainder / 100_000L);
       }
       toAppendTo.append('m');
     }
